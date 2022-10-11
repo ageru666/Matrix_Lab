e9cae2a42329839743b7a3fa39bd3c3014661661
hoksly <realhoksly@gmail.com>
2022-10-11 18:26:09 +0300

    Future methods added

--- Matrix.h
+++ Matrix.h
@@ -11,17 +11,34 @@ protected:
     size_t _height;
     size_t _width;
 
+    // methods
+
 public:
+    // Getters
+    inline size_t height() { return _height; }
+    inline size_t width() { return _width; }
     inline std::complex<double> *operator[](size_t i)
     {
         assert(i < _height);
         return matrix[i];
     };
-    inline size_t height() { return _height; }
-    inline size_t width() { return _width; }
 
+    // Constructors
     Matrix(size_t a, size_t b);
+    Matrix(size_t a, size_t b, bool random_init);
+    Matrix(size_t a, size_t b, std::pair<double, double> **M);
+    Matrix(std::string filename);
+
+    // Destructor
     ~Matrix();
+
+    // Operators overload
+    Matrix operator+(const Matrix &);
+    Matrix operator-(const Matrix &);
+    Matrix operator*(const Matrix &);
+
+    // Other methods
+    std::complex<double> D();
 };
 
 #endif
