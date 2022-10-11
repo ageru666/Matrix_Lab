a0de79047738fcd630fcb8bc7738672223125a47
hoksly <realhoksly@gmail.com>
2022-10-11 18:20:34 +0300

    getters added, protected variables updated

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,18 +3,21 @@
 
 Matrix::Matrix(size_t a, size_t b)
 {
+    if (a < 1 || b < 1)
+        throw std::invalid_argument("Matrix width and height must be 1 or larger");
+
     matrix = new std::complex<double> *[a];
     for (size_t i = 0; i < a; i++)
         matrix[i] = new std::complex<double>[b];
 
-    height = a;
-    width = b;
+    _height = a;
+    _width = b;
 }
 
 Matrix::~Matrix()
 {
 
-    for (size_t i = 0; i < height; i++)
+    for (size_t i = 0; i < _height; i++)
         delete matrix[i];
 
     delete matrix;
--- Matrix.h
+++ Matrix.h
@@ -6,17 +6,19 @@
 
 class Matrix
 {
-private:
+protected:
     std::complex<double> **matrix;
-    size_t height;
-    size_t width;
+    size_t _height;
+    size_t _width;
 
 public:
     inline std::complex<double> *operator[](size_t i)
     {
-        assert(i < height);
+        assert(i < _height);
         return matrix[i];
     };
+    inline size_t height() { return _height; }
+    inline size_t width() { return _width; }
 
     Matrix(size_t a, size_t b);
     ~Matrix();
